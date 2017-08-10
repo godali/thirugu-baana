e6d99905d21fe8dda86b06ee1fbb483af3b5340f
abhijith-prabhakar <abhi.predator@gmail.com>
2017-08-09 21:05:14 -0400

    initial commit -yipeeeee

---
+++ src/main/java/com/godali/thirugubaana/ThirugubaanaApplication.java
@@ -0,0 +1,12 @@
+package com.godali.thirugubaana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirugubaanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirugubaanaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/godali/thirugubaana/ThirugubaanaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.godali.thirugubaana;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThirugubaanaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
