3f624b690fab92475c1ebade44da0addf746fee5
abhijith-prabhakar <abhi.predator@gmail.com>
2017-08-11 08:28:34 -0400

    adding basic controller

--- src/test/java/com/godali/thirugubaana/ThirugubaanaApplicationTests.java
+++ src/test/java/com/godali/thirugubaana/ThirugubaanaApplicationTests.java
@@ -1,16 +1,26 @@
 package com.godali.thirugubaana;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.assertj.core.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.testng.annotations.Test;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ThirugubaanaApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testHome() throws Exception {
+        ResponseEntity<String> entity = this.restTemplate.getForEntity("/greeting", String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody()).isEqualTo("Hello World");
+    }
 
 }
